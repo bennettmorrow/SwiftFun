964a0a3ee767e4aaa040ac5445f42d218ed97827
Unknown <bennett@bennettmorrow.com>
2018-02-16 23:49:49 -0500

    Added additon switch
    Created addition switch to choose between addition and subtraction

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -18,18 +18,26 @@ class ViewController: UIViewController {
 //    var buttonCount = 0
     
     @IBOutlet weak var myLabel: UILabel!
-    
-    
     @IBOutlet weak var topTextField: UITextField!
-    
-    
     @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
-
-        let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
         
-        myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+
+        
         
 /*
         buttonCount = buttonCount + 1
